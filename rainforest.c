f4db4ec420d12dd0398528e02bb888baca8c71c7
Luke <luke@clashproject.com>
2019-03-30 14:06:23 +0100

    Cleaned up print256

--- rainforest.c
+++ rainforest.c
@@ -606,13 +606,8 @@ void rf256_hash2(void *out, const void *in, size_t len, uint32_t seed) {
 
 #ifdef RAINFOREST_TEST
 static void print256(const uint8_t *b, const char *tag) {
-  printf("%s: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x"
-	 ".%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
-         tag,
-         b[0],  b[1],  b[2],  b[3],  b[4],  b[5],  b[6],  b[7],
-         b[8],  b[9],  b[10], b[11], b[12], b[13], b[14], b[15],
-         b[16], b[17], b[18], b[19], b[20], b[21], b[22], b[23],
-         b[24], b[25], b[26], b[27], b[28], b[29], b[30], b[31]);
+	uint64_t* b_64 = (uint64_t*)b;
+  printf("%s: %016jx%016jx.%016jx%016jx",b_64[0],b_64[1],b_64[2],b_64[3]);
 }
 
 int main(int argc, char **argv) {
