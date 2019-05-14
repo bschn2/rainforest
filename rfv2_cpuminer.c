55dc11bf2c164934ad368b5f538fdccf1529d016
bschn2 <39671501+bschn2@users.noreply.github.com>
2019-05-14 22:50:24 +0200

    update the cpuminer's scan function to skip hashes requiring more than 2 rounds
    /!\ WARNING /!\  The reported hash rate is wrong now, as it covers all hashes including those we have not attempted. Since we're doing 1/256 of the values, the real effective hash rate is only 1/256 of what is reported.

--- rfv2_cpuminer.c
+++ rfv2_cpuminer.c
@@ -32,11 +32,12 @@ int scanhash_rfv2(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *h
 	const uint32_t first_nonce = pdata[19];
 	uint32_t nonce = first_nonce;
 	volatile uint8_t *restart = &(work_restart[thr_id].restart);
+	uint32_t msgh, msgh_init;
 	void *rambox;
 	int ret = 0;
 
 	if (opt_benchmark)
-		Htarg = ptarget[7] = 0x1ffffff;
+		Htarg = ptarget[7] = 0x1ffff;
 
 	//printf("thd%d work=%p htarg=%08x ptarg7=%08x first_nonce=%08x max_nonce=%08x hashes_done=%Lu\n",
 	//       thr_id, work, Htarg, ptarget[7], first_nonce, max_nonce, (unsigned long)*hashes_done);
@@ -50,9 +51,15 @@ int scanhash_rfv2(int thr_id, struct work *work, uint32_t max_nonce, uint64_t *h
 
 	rfv2_raminit(rambox);
 	// pre-compute the hash state based on the constant part of the header
+	msgh_init = rf_crc32_mem(0, endiandata, 76);
 
 	do {
 		be32enc(&endiandata[19], nonce);
+#ifndef RFV2_TRY_ALL_HASHES
+		msgh = rf_crc32_mem(msgh_init, &endiandata[19], 4);
+		if (sin_scaled(msgh) != 2)
+			goto next;
+#endif
 		rfv2_hash(hash, endiandata, 80, rambox, NULL);
 
 		if (hash[7] <= Htarg && fulltest(hash, ptarget)) {
